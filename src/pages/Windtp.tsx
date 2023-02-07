b24e0314d0b967757e523b9d93c47a726ddca296
Kyoeun Lee <lke159357@hufs.ac.kr>
2023-02-07 23:34:48 +0900

    Add: Tem1 (Temperature) Page 추가

--- src/pages/Windtp.tsx
+++ src/pages/Windtp.tsx
@@ -1,1 +1,14 @@
-export {};
+import React from "react";
+import Footer from "../components/Footer";
+import HeaderMy1 from "../components/HeaderMy1";
+
+function Windtp() {
+  return (
+    <div>
+      
+           
+    </div>
+  );
+}
+
+export default Windtp;
